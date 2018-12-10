29e61f15814add05913f101884dd759fcb5a0604
Sylvain <sylvan.jalbert@gmail.com>
2018-12-10 22:18:30 +0100

    creation des vues et définition des routes dans le controller

--- src/Controller/ProstageController.php
+++ src/Controller/ProstageController.php
@@ -8,12 +8,45 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProstageController extends AbstractController
 {
     /**
-     * @Route("/prostage", name="prostage")
+     * @Route("/", name="prostage_accueil")
      */
     public function index()
     {
         return $this->render('prostage/index.html.twig', [
             'controller_name' => 'ProstageController',
+        ]);
+    }
+
+    /**
+     * @Route("/filtrer/{id}", name="prostage_filtre")
+     */
+    public function afficherFiltres($id)
+    {
+        return $this->render('prostage/affichageFiltre.html.twig', [
+            'controller_name' => 'ProstageController',
+            'idFiltre' => $id,
+        ]);
+    }
+
+    /**
+     * @Route("/resultatRecherche/{filtre}", name="prostage_resultatRecherche")
+     */
+    public function afficherResultatRecherche($filtre)
+    {
+        return $this->render('prostage/affichageResultatRecherche.html.twig', [
+            'controller_name' => 'ProstageController',
+            'idFiltre' => $filtre,
+        ]);
+    }
+
+    /**
+     * @Route("/stage/{id}", name="prostage_stage")
+     */
+    public function afficherStage($id)
+    {
+        return $this->render('prostage/affichageStage.html.twig', [
+            'controller_name' => 'ProstageController',
+            'idStage' => $id,
         ]);
     }
 }
